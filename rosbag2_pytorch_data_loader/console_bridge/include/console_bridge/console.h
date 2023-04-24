c3f8b00fe5e3a0a71a2b907ff82e9a6608d163dd
Masaya Kataoka <ms.kataoka@gmail.com>
2023-04-25 00:58:11 +0900

    remove include
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- rosbag2_pytorch_data_loader/console_bridge/include/console_bridge/console.h
+++ rosbag2_pytorch_data_loader/console_bridge/include/console_bridge/console.h
@@ -39,7 +39,42 @@
 
 #include <string>
 
-#include "./console_bridge_export.h"
+#ifdef CONSOLE_BRIDGE_STATIC_DEFINE
+#  define CONSOLE_BRIDGE_DLLAPI
+#  define CONSOLE_BRIDGE_NO_EXPORT
+#else
+#  ifndef CONSOLE_BRIDGE_DLLAPI
+#    ifdef console_bridge_EXPORTS
+        /* We are building this library */
+#      define CONSOLE_BRIDGE_DLLAPI __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define CONSOLE_BRIDGE_DLLAPI __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef CONSOLE_BRIDGE_NO_EXPORT
+#    define CONSOLE_BRIDGE_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef CONSOLE_BRIDGE_DEPRECATED
+#  define CONSOLE_BRIDGE_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef CONSOLE_BRIDGE_DEPRECATED_EXPORT
+#  define CONSOLE_BRIDGE_DEPRECATED_EXPORT CONSOLE_BRIDGE_DLLAPI CONSOLE_BRIDGE_DEPRECATED
+#endif
+
+#ifndef CONSOLE_BRIDGE_DEPRECATED_NO_EXPORT
+#  define CONSOLE_BRIDGE_DEPRECATED_NO_EXPORT CONSOLE_BRIDGE_NO_EXPORT CONSOLE_BRIDGE_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef CONSOLE_BRIDGE_NO_DEPRECATED
+#    define CONSOLE_BRIDGE_NO_DEPRECATED
+#  endif
+#endif
 
 /** \file console.h
     \defgroup logging Logging Macros
