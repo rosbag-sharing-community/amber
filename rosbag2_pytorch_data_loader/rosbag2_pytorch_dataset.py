8403585e51162b8899a89e2ecae6cc69e31f3fb5
Masaya Kataoka <ms.kataoka@gmail.com>
2023-04-27 22:55:51 +0900

    add dataset class
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ rosbag2_pytorch_data_loader/rosbag2_pytorch_dataset.py
@@ -0,0 +1,21 @@
+import os
+from torch.utils.data import Dataset
+from typing import Any
+
+
+class CustomImageDataset(Dataset):  # type: ignore
+    def __init__(
+        self,
+        rosbag_path: str,
+        dataset_yaml_path: str,
+        transform: Any = None,
+        target_transform: Any = None,
+    ) -> None:
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self) -> int:
+        return 1
+
+    def __getitem__(self, idx: int) -> Any:
+        return []
