c2368857c302d3848d40cc286522abf6b11bbde5
Masaya Kataoka <ms.kataoka@gmail.com>
2023-05-23 12:46:02 +0900

    remove clip_image_filter
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- rosbag2_pytorch_data_loader/automation/task_description.py
+++ rosbag2_pytorch_data_loader/automation/task_description.py
@@ -5,51 +5,6 @@ from enum import Enum
 from typing import Dict
 
 
-class ClipModelType(Enum):
-    RN50 = "RN50"
-    RN101 = "RN101"
-    RN50x4 = "RN50x4"
-    RN50x16 = "RN50x16"
-    RN50x64 = "RN50x64"
-    ViT_B_32 = "ViT-B/32"
-    ViT_B_16 = "ViT-B/16"
-    ViT_L_14 = "ViT-L/14"
-    ViT_L_14_at_336px = "ViT-L/14@336px"
-
-
-@dataclass
-class TargetObjectsConfig(YAMLWizard):  # type: ignore
-    label: str = ""
-    threshold: float = 0.5
-    positive_prompt_prefix = "A photo of "
-    negative_prompt_prefix = "Not a photo of"
-
-
-@dataclass
-class ClipImageFilterConfig(YAMLWizard):  # type: ignore
-    task_type: str = "clip_image_filter"
-    model: ClipModelType = ClipModelType.ViT_B_32
-    target_objects: list[TargetObjectsConfig] = field(default_factory=list)
-
-    def get_prompts(self) -> list[tuple[str, str]]:
-        prompts = []
-        for target_object in self.target_objects:
-            prompts.append(
-                (
-                    target_object.positive_prompt_prefix + target_object.label,
-                    target_object.negative_prompt_prefix + target_object.label,
-                )
-            )
-        return prompts
-
-
-@dataclass
-class ImageClassification(JSONWizard):  # type: ignore
-    topic: str = ""
-    sequence: int = 0
-    labels: list[str] = field(default_factory=list)
-
-
 class DeticModelType(Enum):
     Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max_size = (
         "Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size"
--- tests/test_automation.py
+++ tests/test_automation.py
@@ -5,18 +5,6 @@ import os
 from rosbag2_pytorch_data_loader.dataset.rosbag2_pytorch_dataset import Rosbag2Dataset
 
 
-def test_clip_image_filter() -> None:
-    current_path = Path(os.path.dirname(os.path.realpath(__file__)))
-    filter = ClipImageFilter(str(current_path / "clip_image_filter.yaml"))
-    dataset = Rosbag2Dataset(
-        str(current_path / "rosbag" / "ford" / "ford.mcap"),
-        str(current_path / "read_image_ford.yaml"),
-    )
-    annotation = filter.inference(dataset)
-    filter.write(dataset, "clip_image_filter", annotation, "output.mcap")
-    os.remove("output.mcap")
-
-
 def test_detic_auto_labeler() -> None:
     current_path = Path(os.path.dirname(os.path.realpath(__file__)))
     labeler = DeticImageLabeler(str(current_path / "detic_image_labeler.yaml"))
