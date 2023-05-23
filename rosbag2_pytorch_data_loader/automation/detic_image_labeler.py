1ddf23c5ce51808b073e17a39503411a9206c7d8
Masaya Kataoka <ms.kataoka@gmail.com>
2023-05-23 11:00:33 +0900

    enable write video
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- rosbag2_pytorch_data_loader/automation/detic_image_labeler.py
+++ rosbag2_pytorch_data_loader/automation/detic_image_labeler.py
@@ -17,6 +17,10 @@ import os
 import docker
 import socket
 from torchvision import transforms
+import cv2
+import time
+from tqdm import tqdm
+from typing import Any
 
 
 class DemoArguments:
@@ -26,24 +30,19 @@ class DemoArguments:
 
 
 class DeticImageLabeler(Automation):  # type: ignore
-    models = {
-        "Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size": {
-            "filename": "Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth",
-            "url": "https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth",
-        }
-    }
-
     def __init__(self, yaml_path: str) -> None:
         self.temporary_image_filepath = "/tmp/input.jpg"
         self.to_pil_image = transforms.ToPILImage()
         self.config = DeticImageLabalerConfig.from_yaml_file(yaml_path)
         self.docker_client = docker.from_env()
         self.container = self.docker_client.containers.run(
-            "detic", detach=True, network_mode="host"
+            "wamvtan/detic", detach=True, network_mode="host"
         )
         # waiting for until the docker container is ready
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while sock.connect_ex(("0.0.0.0", 8000)) != 0:
+            print("Try connecting to container...")
+            time.sleep(1)
             continue
         self.client = Client("http://0.0.0.0:8000")
 
@@ -51,7 +50,21 @@ class DeticImageLabeler(Automation):  # type: ignore
         self.container.stop()
 
     def inference(self, dataset: Rosbag2Dataset) -> None:
+        images = []
+        video: Any = None
+        bar = tqdm(total=len(dataset))
+        bar.set_description("Annotation progress")
         for index, image in enumerate(dataset):
             self.to_pil_image(image).save(self.temporary_image_filepath)
-            print(self.client.predict(self.temporary_image_filepath))
-            # Image.open()
+            images.append(self.client.predict(self.temporary_image_filepath))
+            bar.update()
+        for image in images:
+            opencv_image = cv2.imread(image)
+            if video == None:
+                video = cv2.VideoWriter(
+                    "output.mp4",
+                    cv2.VideoWriter_fourcc("m", "p", "4", "v"),
+                    30.0,  # FPS
+                    (opencv_image.shape[1], opencv_image.shape[0]),
+                )
+            video.write(opencv_image)
