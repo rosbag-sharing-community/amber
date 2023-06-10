110e3f7a142e9abe014d72d8ee0287df957e55ca
Masaya Kataoka <ms.kataoka@gmail.com>
2023-06-10 09:00:25 +0000

    fix test
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -5,14 +5,14 @@ import os
 from amber.dataset.rosbag2_dataset import Rosbag2Dataset
 
 
-# def test_detic_auto_labeler() -> None:
-#     current_path = Path(os.path.dirname(os.path.realpath(__file__)))
-#     labeler = DeticImageLabeler(str(current_path / "detic_image_labeler.yaml"))
-#     dataset = Rosbag2Dataset(
-#         str(current_path / "rosbag" / "ford" / "ford.mcap"),
-#         str(current_path / "read_image_ford.yaml"),
-#     )
-#     labeler.inference(dataset)
+def test_detic_auto_labeler() -> None:
+    current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    labeler = DeticImageLabeler(str(current_path / "detic_image_labeler.yaml"))
+    dataset = Rosbag2Dataset(
+        str(current_path / "rosbag" / "ford" / "ford.mcap"),
+        str(current_path / "read_image_ford.yaml"),
+    )
+    labeler.inference(dataset)
 
 
 # @pytest.mark.slow
