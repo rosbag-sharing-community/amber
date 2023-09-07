081a89160998d0b9ac1300b3bb9f56656e8ff060
Masaya Kataoka <ms.kataoka@gmail.com>
2023-09-08 00:37:16 +0900

    enable embed clip features for each bounding box
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- amber/automation/clip.py
+++ amber/automation/clip.py
@@ -1,6 +1,7 @@
 import torch
 from clip.clip import load, tokenize
 from torchvision import transforms
+from amber.automation.annotation import ImageAnnotation, BoundingBoxAnnotation
 
 
 class ClipEncoder:
@@ -9,12 +10,32 @@ class ClipEncoder:
         self.model, self.preprocess = load("ViT-B/32", device=self.device)
         self.to_pil_image = transforms.ToPILImage()
 
-    def get_image_features(self, image: torch.Tensor) -> torch.Tensor:
+    def get_image_embeddings(self, image: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():
             return self.model.encode_image(
                 self.preprocess(self.to_pil_image(image)).unsqueeze(0).to(self.device)
             )
 
-    def get_text_features(self, text: str) -> torch.Tensor:
+    def get_image_embeddings_for_objects(
+        self, image: torch.Tensor, annotation: ImageAnnotation
+    ) -> ImageAnnotation:
+        with torch.no_grad():
+            pil_image = self.to_pil_image(image)
+            for bounding_box in annotation.bounding_boxes:
+                bounding_box.clip_embeddings = self.model.encode_image(
+                    self.preprocess(
+                        pil_image.crop(
+                            int(bounding_box.x1),
+                            int(bounding_box.y1),
+                            int(bounding_box.x2),
+                            int(bounding_box.y2),
+                        )
+                    )
+                    .unsqueeze(0)
+                    .to(self.device)
+                ).tolist()[0]
+        return annotation
+
+    def get_text_embeddings(self, text: str) -> torch.Tensor:
         with torch.no_grad():
             return self.model.encode_text(tokenize([text]).to(self.device))
