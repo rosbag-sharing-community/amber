b5873b927b4b47dd242074b3f0cbb85f1b55db92
Masaya Kataoka <ms.kataoka@gmail.com>
2023-11-28 06:09:09 +0900

    add test case for speed limit checking
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -16,3 +16,14 @@ def test_blip2_encoder() -> None:
     ) > encoder.get_cosine_similarity_from_image_file_and_text(
         image_of_green_traffic_light, "The color of traffic light is red."
     )
+    image_of_speed_limit: Path = Path(current_path / "images" / "ford" / "35.png")
+    assert encoder.get_cosine_similarity_from_image_file_and_text(
+        image_of_speed_limit, "The speed limit is 35."
+    ) > encoder.get_cosine_similarity_from_image_file_and_text(
+        image_of_speed_limit, "The speed limit is 20."
+    )
+    assert encoder.get_cosine_similarity_from_image_file_and_text(
+        image_of_speed_limit, "The speed limit is 35."
+    ) > encoder.get_cosine_similarity_from_image_file_and_text(
+        image_of_speed_limit, "The speed limit is 40."
+    )
