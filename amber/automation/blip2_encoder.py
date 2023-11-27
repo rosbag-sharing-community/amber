0afad793b83c8a9fc928869bcbdf46f29437a26a
Masaya Kataoka <ms.kataoka@gmail.com>
2023-11-28 06:03:42 +0900

    add test case
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- amber/automation/blip2_encoder.py
+++ amber/automation/blip2_encoder.py
@@ -82,6 +82,22 @@ class Blip2Encoder:
             self.image_processor(transforms.ToTensor()(Image.open(image_path)))
         )
 
+    def get_cosine_similarity_from_image_and_text(
+        self, image: torch.Tensor, text: str
+    ) -> float:
+        text_embedding = self.encode_text(text)
+        image_embedding = self.encode_image(image)
+        sim: float = 0.0
+        sim, _ = torch.max(torch.bmm(image_embedding, text_embedding.unsqueeze(-1)))
+        return sim
+
+    def get_cosine_similarity_from_image_file_and_text(
+        self, image_path: Path, text: str
+    ) -> float:
+        return self.get_cosine_similarity_from_image_and_text(
+            self.image_processor(transforms.ToTensor()(Image.open(image_path))), text
+        )
+
 
 if __name__ == "__main__":
     pass
--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -8,14 +8,11 @@ import torch
 def test_blip2_encoder() -> None:
     current_path = Path(os.path.dirname(os.path.realpath(__file__)))
     encoder = Blip2Encoder()
-    image_embedding = encoder.encode_image_from_file(
-        Path(current_path / "images" / "ford" / "26.png")
+    image_of_green_traffic_light: Path = Path(
+        current_path / "images" / "ford" / "26.png"
     )
-    text_embedding_green = encoder.encode_text("The color of traffic light is green.")
-    text_embedding_red = encoder.encode_text("The color of traffic light is red.")
-    # sims = torch.bmm(image_embedding, text_embedding.unsqueeze(-1))
-    sims_green = torch.bmm(image_embedding, text_embedding_green.unsqueeze(-1))
-    sim_green, _ = torch.max(sims_green, dim=1)
-    sims_red = torch.bmm(image_embedding, text_embedding_red.unsqueeze(-1))
-    sim_red, _ = torch.max(sims_red, dim=1)
-    assert sim_green > sim_red
+    assert encoder.get_cosine_similarity_from_image_file_and_text(
+        image_of_green_traffic_light, "The color of traffic light is green."
+    ) > encoder.get_cosine_similarity_from_image_file_and_text(
+        image_of_green_traffic_light, "The color of traffic light is red."
+    )
