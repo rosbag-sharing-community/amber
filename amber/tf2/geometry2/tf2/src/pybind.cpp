49cb32909ba2fad358f0ace2100451b5e24e86fa
Masaya Kataoka <ms.kataoka@gmail.com>
2024-12-22 20:51:00 +0900

    merge .cpp file
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- amber/tf2/geometry2/tf2/src/pybind.cpp
+++ amber/tf2/geometry2/tf2/src/pybind.cpp
@@ -14,3 +14,39 @@
 
 #include <tf2/buffer_core.h>
 #include <pybind11/pybind11.h>
+
+PYBIND11_MODULE(tf2_amber, m)
+{
+    m.doc() = "python package compatible with tf2";
+    namespace py = pybind11;
+
+    py::class_<builtin_interfaces::msg::Time>(m, "Time")
+        .def_readwrite("sec", &builtin_interfaces::msg::Time::sec)
+        .def_readwrite("nanosec", &builtin_interfaces::msg::Time::nanosec);  
+
+    py::class_<std_msgs::msg::Header>(m, "Header")
+        .def_readwrite("seq", &std_msgs::msg::Header::seq)
+        .def_readwrite("stamp", &std_msgs::msg::Header::stamp)
+        .def_readwrite("frame_id", &std_msgs::msg::Header::frame_id);
+
+    py::class_<geometry_msgs::msg::Vector3>(m, "Vector3")
+        .def_readwrite("x", &geometry_msgs::msg::Vector3::x)
+        .def_readwrite("y", &geometry_msgs::msg::Vector3::y)
+        .def_readwrite("z", &geometry_msgs::msg::Vector3::z);
+
+    py::class_<geometry_msgs::msg::Quaternion>(m, "Quaternion")
+        .def_readwrite("x", &geometry_msgs::msg::Quaternion::x)
+        .def_readwrite("y", &geometry_msgs::msg::Quaternion::y)
+        .def_readwrite("z", &geometry_msgs::msg::Quaternion::z)
+        .def_readwrite("z", &geometry_msgs::msg::Quaternion::w);
+
+    py::class_<geometry_msgs::msg::Transform>(m, "Transform")
+        .def_readwrite("translation", &geometry_msgs::msg::Transform::translation)
+        .def_readwrite("rotation", &geometry_msgs::msg::Transform::rotation);
+
+    py::class_<geometry_msgs::msg::TransformStamped>(m, "TransformStamped")
+        .def_readwrite("header", &geometry_msgs::msg::TransformStamped::header)
+        .def_readwrite("child_frame_id", &geometry_msgs::msg::TransformStamped::child_frame_id)
+        .def_readwrite("transform", &geometry_msgs::msg::TransformStamped::transform);
+}
+
