ce8566b9de2d496abfb23dcca84a45401c1e5649
Masaya Kataoka <ms.kataoka@gmail.com>
2024-12-26 08:45:53 +0900

    add __iter__ and __len__ function
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- amber/dataset/tf_dataset.py
+++ amber/dataset/tf_dataset.py
@@ -2,19 +2,23 @@ from amber.dataset.rosbag2_dataset import Rosbag2Dataset, MessageMetaData
 from amber.dataset.topic_config import TfTopicConfig
 from dataclass_wizard import YAMLWizard
 from tf2_amber import BufferCore, durationFromSec, TransformStamped
+from dataclasses import dataclass
+import torch
+import sys
+from mcap.reader import NonSeekingReader
 
 
 @dataclass
 class ReadTfTopicConfig(YAMLWizard):  # type: ignore
-    tf_topics: TfTopicConfig = TfTopicConfig()
+    tf_topic: TfTopicConfig = TfTopicConfig()
     compressed: bool = True
-    sampling_duration: double = 0.01666666666  # 60Hz
+    sampling_duration: float = 0.01666666666  # 60Hz
 
     def get_tf_topic(self) -> str:
-        return self.tf_topics.topic_name
+        return self.tf_topic.topic_name
 
     def get_static_tf_topic(self) -> str:
-        return self.tf_topics.static_tf_topic_name
+        return self.tf_topic.static_tf_topic_name
 
 
 class TfDataset(Rosbag2Dataset):  # type: ignore
@@ -29,11 +33,7 @@ class TfDataset(Rosbag2Dataset):  # type: ignore
             rosbag_path,
             self.config.compressed,
         )
-        self.tf_buffer = BufferCore(
-            durationFromSec(
-                (self.get_last_timestamp - self.get_first_timestamp()).total_seconds()
-            )
-        )
+        self.tf_buffer = BufferCore(durationFromSec(sys.float_info.max))
         for rosbag_file in self.rosbag_files:
             reader = NonSeekingReader(rosbag_file)
             for schema, channel, message in reader.iter_messages():
@@ -42,3 +42,9 @@ class TfDataset(Rosbag2Dataset):  # type: ignore
                     pass
                 if channel.topic in [self.config.get_static_tf_topic()]:
                     pass
+
+    def __len__(self) -> int:
+        return 0
+
+    def __iter__(self) -> torch.Tensor:
+        torch.zeros(0)
