847041f9c922b68f647de5d428bd9820f909607a
Masaya Kataoka <ms.kataoka@gmail.com>
2025-04-16 22:59:05 +0900

    add encode text / image
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- amber_mcap/automation/blip2_encoder.py
+++ amber_mcap/automation/blip2_encoder.py
@@ -33,95 +33,27 @@ class Blip2Encoder:
             .item()
         )
 
-    # def preprocess_image_embeddings(
-    #     self, image: torch.Tensor
-    # ) -> Tuple[torch.Tensor, torch.Tensor]:
-    #     image = self.image_processor(image)
-    #     assert image.dim() == 3
-    #     image = image.unsqueeze(0).to(self.device)
-    #     with self.model.maybe_autocast():
-    #         image_embeds = self.model.ln_vision(self.model.visual_encoder(image))
-    #     image_embeds = image_embeds.float()
-    #     image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(
-    #         image.device
-    #     )
-    #     return (image_embeds, image_atts)
+    def encode_text(self, text: str) -> torch.Tensor:
+        inputs = processor(text=texts, return_tensors="pt").to(
+            self.device, torch.float16
+        )
+        itc_out = model(**inputs, use_image_text_matching_head=False)
+        if not itc_out.text_embeds:
+            raise Exception("Failed to get text embeddings")
+        return itc_out.text_embeds
 
-    # https://github.com/salesforce/LAVIS/blob/506965b9c4a18c1e565bd32acaccabe0198433f7/lavis/models/blip2_models/blip2_image_text_matching.py#L71-L89
-    # def get_itm_score(self, image: torch.Tensor, text: str) -> float:
-    #     image_embeds, image_atts = self.preprocess_image_embeddings(image)
-    #     text_tensor: torch.Tensor = self.get_text_tensor(text)
-    #     query_tokens = self.model.query_tokens.expand(image_embeds.shape[0], -1, -1)
-    #     query_atts = torch.ones(query_tokens.size()[:-1], dtype=torch.long).to(
-    #         text_tensor["input_ids"].device
-    #     )
-    #     attention_mask = torch.cat([query_atts, text_tensor.attention_mask], dim=1)
-    #     output_itm = self.model.Qformer.bert(
-    #         text_tensor.input_ids,
-    #         query_embeds=query_tokens,
-    #         attention_mask=attention_mask,
-    #         encoder_hidden_states=image_embeds,
-    #         encoder_attention_mask=image_atts,
-    #         return_dict=True,
-    #     )
-    #     itm_embeddings = output_itm.last_hidden_state[:, : query_tokens.size(1), :]
-    #     itm_logit = self.model.itm_head(itm_embeddings)
-    #     itm_logit = itm_logit.mean(dim=1)
-    #     return float(itm_logit[:, 1].item())
+    def encode_image(self, image: torch.Tensor) -> torch.Tensor:
+        image_fp16 = image.new_tensor(image, dtype=torch.float16, device=self.device)
+        inputs = processor(images=image_fp16, return_tensors="pt").to(
+            self.device, torch.float16
+        )
+        itc_out = model(**inputs, use_image_text_matching_head=False)
+        if not itc_out.image_embeds:
+            raise Exception("Failed to get image embeddings")
+        return itc_out.image_embeds
 
-    # def get_text_tensor(self, text: str) -> BatchEncoding:
-    #     text_tensor: BatchEncoding = self.model.tokenizer(
-    #         text,
-    #         truncation=True,
-    #         max_length=self.model.max_txt_len,
-    #         return_tensors="pt",
-    #     ).to(self.device)
-    #     return text_tensor
-
-    # def encode_image(self, image: torch.Tensor) -> torch.Tensor:
-    #     image_embeds, image_atts = self.preprocess_image_embeddings(image)
-    #     query_tokens = self.model.query_tokens.expand(image_embeds.shape[0], -1, -1)
-    #     query_output = self.model.Qformer.bert(
-    #         query_embeds=query_tokens,
-    #         encoder_hidden_states=image_embeds,
-    #         encoder_attention_mask=image_atts,
-    #         return_dict=True,
-    #     )
-    #     return F.normalize(
-    #         self.model.vision_proj(query_output.last_hidden_state), dim=-1
-    #     )
-
-    # def encode_text(self, text: str) -> torch.Tensor:
-    #     text_tensor: torch.Tensor = self.get_text_tensor(text)
-    #     text_output = self.model.Qformer.bert(
-    #         text_tensor.input_ids,
-    #         attention_mask=text_tensor.attention_mask,
-    #         return_dict=True,
-    #     )
-    #     return F.normalize(
-    #         self.model.text_proj(text_output.last_hidden_state[:, 0, :]), dim=-1
-    #     )
-
-    # def encode_image_from_file(self, image_path: Path) -> torch.Tensor:
-    #     return self.encode_image(transforms.ToTensor()(Image.open(image_path)))
-
-    # def get_cosine_similarity_from_image_and_text(
-    #     self, image: torch.Tensor, text: str
-    # ) -> float:
-    #     text_embedding = self.encode_text(text)
-    #     image_embedding = self.encode_image(image)
-    #     sim: float = 0.0
-    #     sim, _ = torch.max(
-    #         torch.bmm(image_embedding, text_embedding.unsqueeze(-1)), dim=1
-    #     )
-    #     return sim
-
-    # def get_cosine_similarity_from_image_file_and_text(
-    #     self, image_path: Path, text: str
-    # ) -> float:
-    #     return self.get_cosine_similarity_from_image_and_text(
-    #         self.image_processor(transforms.ToTensor()(Image.open(image_path))), text
-    #     )
+    def encode_image_from_file(self, image_path: Path) -> torch.Tensor:
+        return self.encode_image(transforms.ToTensor()(Image.open(image_path)))
 
 
 if __name__ == "__main__":
--- tests/test_automation.py
+++ tests/test_automation.py
@@ -130,21 +130,3 @@ def test_blip2_encoder_itm() -> None:
         "A white car is on the left lane.",
     )
     assert itm_score_positive > itm_score_negative
-
-
-def test_blip2_encoder_itc() -> None:
-    current_path = Path(os.path.dirname(os.path.realpath(__file__)))
-    enc = Blip2Encoder()
-    itc_score_negative = enc.get_cosine_similarity_from_image_and_text(
-        transforms.ToTensor()(
-            Image.open(str(current_path / "images" / "ford" / "28.png"))
-        ),
-        "Hoge",
-    )
-    itc_score_positive = enc.get_cosine_similarity_from_image_and_text(
-        transforms.ToTensor()(
-            Image.open(str(current_path / "images" / "ford" / "28.png"))
-        ),
-        "A white car is on the left lane.",
-    )
-    assert itc_score_positive > itc_score_negative
